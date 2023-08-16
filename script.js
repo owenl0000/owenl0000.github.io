652958228034ef968ad176b0eac6306f941af667
Owen Liang <owenliang3139@gmail.com>
2023-08-16 18:05:57 -0400

    subheader update

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
+let currentSectionId = null;
 
-document.addEventListener('scroll', function() {
+document.querySelector("#content-wrap").addEventListener('scroll', function() {
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
         if (isInView(section)) {
             const sectionId = section.getAttribute('id');
-            updateIcon(sectionId);
+            console.log(`In view: ${sectionId}`); // Debugging line
+            if (currentSectionId !== sectionId) {
+                updateSectionName(sectionId);
+                currentSectionId = sectionId;
+            }
         }
     });
 });
 
-function isInView(element) {
-    const rect = element.getBoundingClientRect();
-    return (rect.top >= 0 && rect.bottom <= window.innerHeight);
+function getMostVisibleSection(sections) {
+    let maxVisibleHeight = 0;
+    let mostVisibleSection = null;
+
+    sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        const visibleHeight = Math.min(rect.bottom, window.innerHeight) - Math.max(rect.top, 0);
+        if (visibleHeight > maxVisibleHeight) {
+            maxVisibleHeight = visibleHeight;
+            mostVisibleSection = section;
+        }
+    });
+
+    return mostVisibleSection;
 }
 
-function updateIcon(sectionId) {
-    const icons = document.querySelectorAll('.section-icons i');
-    icons.forEach(icon => {
-        icon.style.display = 'none'; 
-    });
-    const iconToShow = document.querySelector(`.section-icons i[data-section="#${sectionId}"]`);
-    if (iconToShow) {
-        iconToShow.style.display = 'inline-block'; 
+document.querySelector("#content-wrap").addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    const mostVisibleSection = getMostVisibleSection(sections);
+    const sectionId = mostVisibleSection.getAttribute('id');
+    if (currentSectionId !== sectionId) {
+        updateSectionName(sectionId);
+        currentSectionId = sectionId;
+    }
+});
+
+
+
+
+function updateSectionName(sectionId) {
+    console.log(`Updating to: ${sectionId}`); // Debugging line
+    const sectionNames = {
+        "welcome": "welcome",
+        "projects": "projects",
+        "skills": "skills",
+        "experience": "experience",
+        "contact": "contact"
+    };
+
+    const sectionNameElement = document.querySelector('.section-name');
+    if (sectionNames[sectionId]) {
+        sectionNameElement.textContent = `\u00A0#${sectionNames[sectionId]}`;
+    } else {
+        sectionNameElement.textContent = `...`;
     }
 }
